6be3b4b66c052ba663e6978ecf0a62284e2bc903
Tanisha <tiakapil5@gmail.com>
2026-05-22 02:43:30 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -166,7 +166,8 @@ app.post("/telegram-webhook", async (req, res) => {
 // =======================
 // Start Server
 // =======================
-const PORT = 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
